f37c058474624f1d663e3ce79f0198528ac174fe
Prashniel Kumar <prashniel@gmail.com>
2024-03-01 11:57:00 +1300

    initial game data

---
+++ data/gameLogic.ts
@@ -0,0 +1,14 @@
+interface Card {
+  val: string
+  bool: string
+  id: string
+}
+
+export default const initalState: Card[] = [
+  {val: "1",bool:"false", id:"a"},
+  {val: "3",bool:"false", id:"b"},
+  {val: "1",bool:"false", id:"c"},
+  {val: "2",bool:"false", id:"d"},
+  {val: "2",bool:"false", id:"e"},
+  {val: "3",bool:"false", id:"f"}
+]
