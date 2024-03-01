ac7fac6b79a892db38a7d0f278a2d4dc5091a469
Kate <63078091+Katie-Davies@users.noreply.github.com>
2024-03-01 13:45:27 +1300

    create gameboard

--- client/components/GameBoard.tsx
+++ client/components/GameBoard.tsx
@@ -2,16 +2,39 @@ import data from '../../data/games'
 
 import { Link } from 'react-router-dom'
 import Card from './Card'
+import { useState } from 'react'
+
+const row1 = ['2', '1', '3']
+const row2 = row1.toSorted()
 
 function GameBoard() {
+  const [number, setNumber] = useState(false)
+
+  function handleClick(number: number) {
+    setNumber(true)
+  }
+
   return (
     <div>
       <h3>React</h3>
       <p>Game Board</p>
       <div className="board">
-        <div className="card">?</div>
-
-        <Card />
+        {row1.map((number) => (
+          <button
+            key={number}
+            className="p-button"
+            onClick={() => {
+              handleClick(number)
+            }}
+          >
+            {number}
+          </button>
+        ))}
+        {row2.map((number) => (
+          <button key={number} className="p-button">
+            {number}
+          </button>
+        ))}
       </div>
     </div>
   )
