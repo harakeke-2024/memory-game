ac32346408c62b0303a0789bc46355af90ab3eea
Prash <155497220+SpcyToast@users.noreply.github.com>
2024-03-01 15:30:53 +1300

    made cards turn white

--- client/components/Card.tsx
+++ client/components/Card.tsx
@@ -1,9 +1,46 @@
+import { useState } from 'react'
 import data from '../../data/games'
-
+import initialState from '../../data/gameLogic.ts'
 import { Link } from 'react-router-dom'
+interface singleCard {
+  val: string
+  bool: boolean
+  id: string
+}
 
-function Card() {
-  return <div className="card">?</div>
+interface Props {
+  val: string
+}
+
+console.log(initialState)
+
+function Card(props: Props) {
+  const [number, setNumber] = useState(initialState)
+  const [color, setColor] = useState('black')
+
+  function handleClick(val: string) {
+    // const checkCard: Card = number.find((ele) => ele.id == id)
+    // const ourCard = number.indexOf(checkCard)
+    // checkCard.bool = true
+    // setNumber(number.splice(ourCard, 1, checkCard))
+    // console.log(number)
+    setColor('white')
+  }
+
+  return (
+    <>
+      <button
+        key={props.val}
+        className="p-button"
+        onClick={() => {
+          handleClick(props.val)
+        }}
+        style={{ background: color }}
+      >
+        {props.val}
+      </button>
+    </>
+  )
 }
 
 export default Card
--- client/components/GameBoard.tsx
+++ client/components/GameBoard.tsx
@@ -5,45 +5,15 @@ import Card from './Card'
 import { useState } from 'react'
 import initialState from '../../data/gameLogic.ts'
 
-interface Card {
-  val: string
-  bool: boolean
-  id: string
-}
-
 function GameBoard() {
-  console.log(initialState)
-  const [number, setNumber] = useState(initialState)
-  const color = ['black', 'white']
-
-  function handleClick(id: string) {
-    const checkCard: Card = number.find((ele) => ele.id == id)
-    const ourCard = number.indexOf(checkCard)
-    checkCard.bool = true
-    setNumber(number.splice(ourCard, 1, checkCard))
-    console.log(number)
-  }
-
   return (
-    <div>
-      <h3>React</h3>
-      <p>Game Board</p>
-      <div className="board">
-        {number.map((card) => (
-          <button
-            key={card.id}
-            className="p-button"
-            onClick={() => {
-              handleClick(card.id)
-            }}
-            style={
-              card.bool ? { background: color[1] } : { background: color[0] }
-            }
-          >
-            {card.val}
-          </button>
-        ))}
-      </div>
+    <div className="board">
+      <Card val="one" />
+      <Card val="two" />
+      <Card val="three" />
+      <Card val="two" />
+      <Card val="three" />
+      <Card val="one" />
     </div>
   )
 }
